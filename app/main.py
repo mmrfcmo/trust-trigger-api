c9d2cc8ad8155243a6a5b50fdb5f18f4f374e5f9
mmrfcmo <mrfcmo@gmail.com>
2026-09-18 19:20:31 +0100

    Add trust report router to main application

--- app/main.py
+++ app/main.py
@@ -70,6 +70,7 @@ app.include_router(extensive_router)
 app.include_router(competitor_router)
 app.include_router(embed_snapshot_router)
 app.include_router(homepage_router)
+from app.api.v1.trust_report_page import router as trust_report_router
 app.include_router(trust_report_router)
 @app.get("/")
 async def root():
